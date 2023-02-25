17f719ba899dd867785f038bc923406e21a8af8e
ayyappan <ayyappandharmavpm@gmail.com>
2023-02-25 17:12:56 +0530

    html and css file added

--- myscript.js
+++ myscript.js
@@ -66,3 +66,5 @@ function load(){
 		xhttp.send();
 
 }
+
+
---
+++ practice.js
@@ -0,0 +1,50 @@
+
+
+
+document.getElementById('samp').addEventListener('mouseenter',function(){
+    this.style.border = '2px solid green'
+})
+
+document.getElementById('samp').addEventListener('mouseleave',function(){
+    this.style.border = 'none'
+})
+
+document.getElementById('samp_1').addEventListener('mouseenter',function(){
+    this.style.border = '2px solid green'
+})
+
+document.getElementById('samp_1').addEventListener('mouseleave',function(){
+    this.style.border = 'none'
+})
+
+document.getElementById('samp_2').addEventListener('mouseenter',function(){
+    this.style.border = '2px solid green'
+})
+
+document.getElementById('samp_2').addEventListener('mouseleave',function(){
+    this.style.border = 'none'
+})
+
+document.getElementById('btn-1').addEventListener('mousedown',function(){
+    this.style.backgroundColor = "purple"
+    this.style.color = "white"
+
+})
+
+document.getElementById('btn-1').addEventListener('mouseleave',function(){
+    this.style.backgroundColor = "white"
+    this.style.color = "black"
+    
+})
+
+document.getElementById('btn-2').addEventListener('mousedown',function(){
+    this.style.backgroundColor = "purple"
+    this.style.color = "white"
+
+})
+
+document.getElementById('btn-2').addEventListener('mouseleave',function(){
+    this.style.backgroundColor = "white"
+    this.style.color = "black"
+    
+})
