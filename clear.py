163cd2e44de596c5b0e8f49120b13d9237070973
ayyappan <ayyappandharmavpm@gmail.com>
2023-03-13 17:23:03 +0530

    clear.py updated

--- clear.py
+++ clear.py
@@ -1,31 +1,46 @@
 import os
 import sys
-count = 0
-#change the directory into downloads
-try :
-  if sys.argv[1] :
-     os.chdir(sys.argv[1])
-except  IndexError :
-     os.chdir(os.path.join(os.environ['HOME'],'Downloads'))
-except :
-   print("invalid path")
-   exit()
 
-try :
-  if sys.argv[2] :
-     extension = f'.{sys.argv[2]}'
-except IndexError :
-     extension = ".pdf"
-
-except :
-  print("something went wrong")
-  exit()
-
-for file in os.listdir() :
-    ext = os.path.splitext(file)[1]
-    if ext == extension :
+def deleteFiles(extension) :
+   count = 0
+   for file in os.listdir() :
+     ext = os.path.splitext(file)[1]
+     if ext == extension :
         os.remove(file)
         count += 1
 
 
-print(f'{count} files deleted sucessfully')
+   print(f'{count} {extension} files deleted sucessfully in {os.getcwd()}')
+
+
+try :
+  args = list(sys.argv)
+
+  #find user path
+  if ( '-p' in args) :
+     index = args.index('-p')
+     curPath = args[index+1]
+  else :
+    curPath = os.path.join(os.environ.get('HOME'),'Downloads')
+
+  if (os.path.exists(curPath)) :
+      os.chdir(curPath)
+  else :
+    raise FileNotFoundError
+
+  #find user extension
+
+  if ('-e' in args) :
+     index = args.index('-e')
+     extension = f'.{args[index + 1]}'
+  else :
+   extension = '.pdf'
+
+  deleteFiles(extension)
+
+except FileNotFoundError :
+   print('Invalid path...')
+
+except :
+  print("something went wrong")
+  exit()
